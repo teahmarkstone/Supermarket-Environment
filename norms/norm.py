1e661d661c836a5c6530780879914ce4ebf7e510
Daniel Kasenberg <daniel.kasenberg@gmail.com>
2021-03-16 17:51:16 -0400

    Still working on norm monitors...

--- norms/norm.py
+++ norms/norm.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import gym
 
 class NormViolation(ABC):
     def __init__(self):
@@ -16,9 +17,33 @@ class Norm(ABC):
     def __init__(self):
         self.known_violations = set()
 
+    def preprocess(self, game, action):
+        pass
+
     @abstractmethod
-    def monitor(self, game):
+    def monitor(self, game, action):
         pass
 
     def reset(self):
         self.known_violations = set()
+
+
+class NormWrapper(gym.Wrapper):
+    def __init__(self, norms, env):
+        super(NormWrapper, self).__init__(env)
+        self.norms = list(norms)
+
+    def step(self, action):
+        for norm in self.norms:
+            norm.preprocess(self.env.game, action)
+        obs, reward, done, info = self.env.step(action)
+        violations = set()
+        for norm in self.norms:
+            violations.update(norm.monitor(self.env.game, action))
+        new_obs = {'violations': violations, 'obs': obs}
+        return new_obs, reward, done, info
+
+    def reset(self, action):
+        super(NormWrapper, self).reset()
+        for norm in self.norms:
+            norm.reset()
--- norms/theft.py
+++ norms/theft.py
@@ -1,14 +1,16 @@
+from enums.player_action import PlayerAction
 from norms.norm import Norm, NormViolation
+from shelves import Shelf
 
 
-class CartTheft(NormViolation):
+class CartTheftViolation(NormViolation):
     def __init__(self, player, cart):
         super().__init__()
         self.thief = player
         self.cart = cart
 
     def __eq__(self, other):
-        return isinstance(other, CartTheft) and (self.thief, self.cart) == (other.thief, other.cart)
+        return isinstance(other, CartTheftViolation) and (self.thief, self.cart) == (other.thief, other.cart)
 
     def __hash__(self):
         return hash((self.thief, self.cart))
@@ -21,12 +23,12 @@ class CartTheftNorm(Norm):
     def __init__(self):
         super(CartTheftNorm, self).__init__()
 
-    def monitor(self, game):
+    def monitor(self, game, action):
         new_violations = set()
         for player in game.players:
             cart = player.curr_cart
             if cart is not None and cart.owner != player:
-                violation = CartTheft(player, cart)
+                violation = CartTheftViolation(player, cart)
                 if violation not in self.known_violations:
                     self.known_violations.add(violation)
                     new_violations.add(violation)
@@ -34,3 +36,119 @@ class CartTheftNorm(Norm):
 
     def reset(self):
         super(CartTheftNorm, self).reset()
+
+
+class ShopliftingViolation(NormViolation):
+    def __init__(self, thief, stolen_food):
+        super(ShopliftingViolation, self).__init__()
+        self.thief = thief
+        self.stolen_food = stolen_food
+
+    def __eq__(self, other):
+        return isinstance(other, ShopliftingViolation) and self.thief == other.thief
+
+    def __hash__(self):
+        return hash(self.thief)
+
+    def as_string(self):
+        return "Player {player} shoplifted".format(player=self.thief)
+
+
+class ShopliftingNorm(Norm):
+    def __init__(self):
+        super(ShopliftingNorm, self).__init__()
+
+    def monitor(self, game, action):
+        new_violations = set()
+        for player in game.players:
+            if player.position[0] > 1:
+                continue
+            stolen_food = []
+            if player.holding_cart is not None:
+                cart = player.holding_cart
+                stolen_food.extend(cart.contents)
+            elif player.holding_food is not None and not player.bought_holding_food:
+                stolen_food.append((player.holding_food, 1))
+            if len(stolen_food) > 0:
+                violation = ShopliftingViolation(player, stolen_food)
+                if violation not in self.known_violations:
+                    self.known_violations.add(violation)
+                    new_violations.add(violation)
+        return new_violations
+    
+    def reset(self):
+        super(ShopliftingNorm, self).reset()
+
+
+class WrongShelfViolation(NormViolation):
+    def __init__(self, player, holding_food, shelf):
+        super(WrongShelfViolation, self).__init__()
+        self.player = player
+        self.shelf = shelf
+        self.holding_food = holding_food
+
+    # No eq or hash implementation; if this isn't the same object, it's not equal
+
+    def as_string(self):
+        return "{player} put the {holding_food} on the {shelf_food} shelf.".format(player=self.player,
+                                                                                   holding_food=self.holding_food,
+                                                                                   shelf_food=self.shelf.string_type)
+
+
+class WrongShelfNorm(Norm):
+    def preprocess(self, game, action):
+        for player in game.players:
+            if player.holding_food is not None and action == PlayerAction.INTERACT:
+                interaction_object = game.interaction_object(player)
+                if isinstance(interaction_object, Shelf) and interaction_object.string_type != player.holding_food:
+                    self.violations.add(WrongShelfViolation(player, player.holding_food, interaction_object))
+
+    def monitor(self, game, action):
+        new_violations = self.violations
+        self.violations = set()
+        return new_violations
+
+    def reset(self):
+        super().reset()
+
+    def __init__(self):
+        super(WrongShelfNorm, self).__init__()
+        self.violations = set()
+
+
+class PlayerCollisionNorm(Norm):
+    def monitor(self, game, action):
+        return set()
+
+    def reset(self):
+        super(PlayerCollisionNorm, self).reset()
+        
+    def __init__(self):
+        super(PlayerCollisionNorm, self).__init__()
+        
+        
+class ObjectCollisionNorm(Norm):
+    def monitor(self, game, action):
+        return set()
+    
+    def reset(self):
+        super(ObjectCollisionNorm, self).reset()
+        
+    def __init__(self):
+        super(ObjectCollisionNorm, self).__init__()
+
+
+class ObstructionNorm(Norm):
+    def monitor(self, game, action):
+        return set()
+
+    def reset(self):
+        super(ObstructionNorm, self).reset()
+
+    def __init__(self):
+        super(ObstructionNorm, self).__init__()
+
+
+class UnattendedCartNorm(Norm):
+    def monitor(self, game, action):
+        pass
